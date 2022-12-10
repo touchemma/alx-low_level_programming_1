42b3e79d900823367eb3c11443e55164af25fad4
touchemma <emmagithub1@gmail.com>
2022-12-10 14:42:47 -0800

    Task_6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - block
- * Description: print all single digit number of base 10
- * starting from 0, following by new line
- * Return : 0
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by new line.
+ * Return: 0
  */
 int main(void)
 {
