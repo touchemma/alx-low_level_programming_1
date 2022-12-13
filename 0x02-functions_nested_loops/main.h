242eca7dd150bc6416632191f4ccddbe4a4d0e53
touchemma <emmagithub1@gmail.com>
2022-12-13 14:36:24 -0800

    Task_0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c)
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +12,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-
+void print_to_98(int n)
 
 #endif
