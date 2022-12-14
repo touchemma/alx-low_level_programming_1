f752f9e77ba7250a613e75598a5f52bdb80993c9
touchemma <emmagithub1@gmail.com>
2022-12-14 01:48:13 -0800

    Task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints alphabet 10 
+ * print_alphabet_x10 - prints alphabet 10 time
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
-	char c; 
+	char c;
 	int i = 0;
 
 	while (i < 10)
