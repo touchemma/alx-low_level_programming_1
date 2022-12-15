0ce17e0b98b4b9bd6bf6e0367b05d8f9943ddf14
touchemma <emmagithub1@gmail.com>
2022-12-15 12:26:34 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * mult - multiple two numbers
- * @a: first number
- * @b: second number
- * Return: result
+ * _isupper - Check if a letter is upper
+ * @x: the number it to be checked
+ *
+ * Return: 1 for upper letter or 0 for any else
  */
-int mult(int a, int b)
+int _isupper(int x)
 {
-	return (a * b);
+	if (x >=65 && x <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
+return (0);
 }
