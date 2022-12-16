ce6410f34258eb2e702de068426a88973c87fe21
touchemma <emmagithub1@gmail.com>
2022-12-16 03:18:16 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -test function that prin if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (o);
+}
