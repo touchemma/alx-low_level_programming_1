1d337508d379490a4524bb01cea36e7370835e1c
touchemma <emmagithub1@gmail.com>
2022-12-15 17:01:54 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _is upper - Check if a letter is upper
- * @x: The number to be checked
- *
+ * _isupper - Check if a letter is upper
+ * @c: the character to be checkied
  * Return: 1 for upper letter or 0 for any else
  */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x<= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
 }
