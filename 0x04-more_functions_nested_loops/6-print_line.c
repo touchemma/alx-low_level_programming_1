561bec568fe4bf7a631d9d05035afc5f1c8ed2b1
touchemma <emmagithub1@gmail.com>
2022-12-15 16:19:02 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ * @n: parameter
+ * Retun: returns nothing
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
