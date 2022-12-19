7a536e7005168dbc8e62fb251c1dfa8c4743c1f5
touchemma <emmagithub1@gmail.com>
2022-12-19 11:35:29 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen -the function that returns the length of a string
+ *
+ * @s: the input string
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+
+	return (index);
+}
