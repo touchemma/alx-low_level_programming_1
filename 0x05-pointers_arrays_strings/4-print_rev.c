1b534f551dbd9aca673fd8d7dd9cd69664263cbf
touchemma <emmagithub1@gmail.com>
2022-12-20 01:43:20 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints astring, in reverse, followed by a line
+ * @s: imput string
+ * Return: no return.
+ */
+
 void print_rev(char *s)
 {
-	int count = 0;
+int count = 0;
 
-	while (count >= 0)
-	{
-		if (s[count] == '0'
-				break
-		count++;
-	}
-
-	for (count--; count >= 0; count --)
-
-	_putchar(s[coup          u utchar('\n')
-
+while (count >= 0)
+{
+if (s[count] == '\0')
+break;
+count++;
+}
+for (count--; count >= 0; count--)
+_putchar(s[count]);
+_putchar('\n');
+}
