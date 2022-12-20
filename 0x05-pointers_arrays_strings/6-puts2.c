cb0b7646ca9e9a8dd8447591db16953a9a60962a
touchemma <emmagithub1@gmail.com>
2022-12-20 09:19:18 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - print one char out of 2 of a string
- * @str: input string
- * Return: no return
+ * puts2 -prints one character out of two
+ * @str: input
+ * Return: print
  */
+void puts2(char *star)
+{
+	int i = 0;
 
-void puts2(char *str)
-{
-int count = 0;
-	
-while (count >= 0)
-{
-if (str[count] == '\0')
-{
-_putchar('\n');
-break;
-}
-if (count % 2 == 0)
-_putchar(str[count]);
-count++;	
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
 }
