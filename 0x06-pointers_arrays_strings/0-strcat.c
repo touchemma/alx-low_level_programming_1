e92cba4c016add3978eee9ad6c376475e022e3ce
touchemma <emmagithub1@gmail.com>
2022-12-22 01:27:55 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatennate two strings
  * @dest: copy to
  * @src: copy from
- * Return: pointer to det(destination)
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
@@ -12,8 +12,12 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-
 	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
