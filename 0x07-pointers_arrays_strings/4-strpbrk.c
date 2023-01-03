27cc246c00fc296514391f4c55f40a6babbfb774
touchemma <emmagithub1@gmail.com>
2023-01-03 07:04:38 -0800

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "oleh";
+
+	n = _strspn(s, f);
+	printf("%u\n", n);
+	return (0);
+}
