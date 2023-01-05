c9bfec8467f85b68514905da32104be81066e8fb
touchemma <emmagithub1@gmail.com>
2023-01-04 23:18:11 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@
  */
 int _evaluate(int i, int n)
 {
-	/* Evaluate function */
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-	else if (i * i == n) /* condition base */
-		return (i);
-	return (-1);
-	return (-1);
+/* Evaluate function */
+if (n == 0 || n == 1)
+return (n);
+else if (i * i < n)
+return (_evaluate(i + 1, n));
+else if (i * i == n) /* condition base */
+return (i);
+return (-1);
+return (-1);
 }
