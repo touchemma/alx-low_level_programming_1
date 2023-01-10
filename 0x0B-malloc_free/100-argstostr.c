468ab993e1b630bca0ce13836b6fec6d12f4d43d
touchemma <emmagithub1@gmail.com>
2023-01-10 07:30:10 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * *argstostr - concatenates all the arguments of the program
  * @ac: number of arguments
@@ -8,34 +9,33 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
+int i, j, k, len;
+char *str;
+if (ac == 0 || av == NULL)
 
-	char *str;
+return (NULL);
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
+for (i = 0; i < ac; i++)
 		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-	return (str);
+for (j = 0; av[i][j] != '\0'; j++)
+len++;
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
+
+if (str == NULL)
+return (NULL);
+
+k = 0;
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+return (str);
 }
