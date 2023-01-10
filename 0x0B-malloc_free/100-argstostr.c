82bb04d8a5ebb53ac8485b201a3c9ea07c21c9a1
touchemma <emmagithub1@gmail.com>
2023-01-10 07:45:07 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,35 +7,33 @@
  * @av: array of arguments
  * Return: Pointer to the new string (Success), NULL (Error)
  */
-char *argstostr(int ac, char **av)
+car *argstostr(int ac, char **av)
 {
-int i, j, k, len;
-char *str;
-if (ac == 0 || av == NULL)
+	int i, j, k, len;
 
-return (NULL);
+	char *str;
 
-for (i = 0; i < ac; i++)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-for (j = 0; av[i][j] != '\0'; j++)
-len++;
-len++;
-}
-str = malloc(sizeof(char) * (len + 1));
-
-if (str == NULL)
-return (NULL);
-
-k = 0;
-
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-str[k] = av[i][j];
-k++;
-}
-str[k] = '\n';
-k++;
-}
-return (str);
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	return (str);
 }
