1bc9f22108ed706186a6a8352e1c81e75bf27ab7
touchemma <emmagithub1@gmail.com>
2023-02-02 06:07:23 -0800

    solution

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,7 +3,7 @@
 * bmain - function exeucted before main
 * Return: no return.
 */
-void _attribute_ ((constructor)) bmain()
+void _attribute_ ((constructor)) bmain();
 {
 printf("You're beat! and yet, you must alllow");
 Printf(",/nI bore my house upon my back!/n";
