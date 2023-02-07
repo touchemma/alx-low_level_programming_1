602c6276b16846dd07f1bba91b424625f45972bc
touchemma <emmagithub1@gmail.com>
2023-02-07 10:20:56 -0800

    answers

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-	for (nletters = 0; text_content[nletters]; nletters++);
+		for (nletters = 0; text_content[nletters]; nletters++);
 
 		rwr = write(fd, text_content, nletters);
 
