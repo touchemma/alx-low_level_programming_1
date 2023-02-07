9ea69fb57d32d3003a321c67ff5cd2bdd0f55474
touchemma <emmagithub1@gmail.com>
2023-02-07 09:51:19 -0800

    answers

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
