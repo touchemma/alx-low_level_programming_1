74c7e32875bfef1537ae807732b8f9f8787b5914
touchemma <emmagithub1@gmail.com>
2023-02-08 08:26:59 -0800

    task ans

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,8 +3,7 @@
 * append_text_to_file - appends text at the end of a file
 * @filename: filename.
 * @text_content: added content.
-* Return: 1 if the file exists. -1 if the fails does not exist
-* or if it fails.
+* Return: 1 if the file exists. -1 if the fails does not exist or if it fails.
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -20,11 +19,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++);
+		for (nletters = 0; text_content[nletters]; nletters++)
 		rwr = write(fd, text_content, nletters);
+
 		if (rwr == -1)
 			return (-1);
 	}
+
 	close(fd);
+
 	return (1);
 }
